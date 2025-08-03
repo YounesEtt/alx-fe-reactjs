bf233be12fc0dd40ce8ab678d9a34cadea6dbdbf
Sigurd <synchasgard@gmail.com>
2025-08-03 15:36:06 +0100

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,14 +1,13 @@
-// src/App.jsx
 import React from 'react';
 import Search from './components/Search';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>GitHub User Search</h1>
+    <div>
+      <h1 style={{ textAlign: 'center' }}>GitHub User Search</h1>
       <Search />
     </div>
   );
-}
+};
 
 export default App;
