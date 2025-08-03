696fb3d403eb2c54a972c7df81a24a59b987a1ff
Sigurd <synchasgard@gmail.com>
2025-08-03 15:45:08 +0100

    githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,27 @@
 import axios from 'axios';
 
-export const fetchUserData = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
-  return response.data;
+export const fetchAdvancedUserSearch = async ({ username, location, minRepos }) => {
+  const queryParts = [];
+
+  if (username) queryParts.push(`${username} in:login`);
+  if (location) queryParts.push(`location:${location}`);
+  if (minRepos) queryParts.push(`repos:>=${minRepos}`);
+
+  const query = queryParts.join(' ');
+
+  const response = await axios.get('https://api.github.com/search/users', {
+    params: {
+      q: query,
+      per_page: 10,
+    },
+  });
+
+  const userDetails = await Promise.all(
+    response.data.items.map(async (user) => {
+      const detailRes = await axios.get(user.url);
+      return detailRes.data;
+    })
+  );
+
+  return userDetails;
 };
