c1c4ffa00f39743c8ddcb16f4b5a661b1733dacd
Sigurd <synchasgard@gmail.com>
2025-08-03 16:08:32 +0100

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
+// Step 1: Basic user fetch function by username
+export const fetchUserData = async (username) => {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
+};
+
+// Existing advanced search function
 export const fetchAdvancedUserSearch = async ({ username, location, minRepos }) => {
   const queryParts = [];
 
